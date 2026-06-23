384e8bb4c683c816b94a6c1d44cbffdef4852147
pontagrossa-ai <pontagrossa@ifpr.edu.br>
2026-06-23 17:40:30 -0300

    btnVoltar

--- app/src/main/java/com/ifpr/wearostemplate/Perfil.kt
+++ app/src/main/java/com/ifpr/wearostemplate/Perfil.kt
@@ -2,11 +2,16 @@ package com.ifpr.wearostemplate
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.widget.Button
 
 class Perfil : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+
+        findViewById<Button>(R.id.btnVoltar).setOnClickListener {
+            finish()
+        }
     }
 }
