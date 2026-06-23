43a0c2092c023dfe60646a964ba16e896ac41fda
pontagrossa-ai <pontagrossa@ifpr.edu.br>
2026-06-23 17:32:07 -0300

    tela de perfil

---
+++ app/src/main/java/com/ifpr/wearostemplate/Perfil.kt
@@ -0,0 +1,12 @@
+package com.ifpr.wearostemplate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class Perfil : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}
